a27813cb93d0c89be70f435d77acc9ba8c00e61a
Jordan <Jordforb@gmail.com>
2022-05-21 02:36:27 -0400

    window opens and runs

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import pygame
 WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def main():
+    
+    run = True 
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False  
+                
+    pygame.quit()
+    
+if __name__ == "__main__":
+    main()
